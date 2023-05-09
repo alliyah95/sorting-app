0f4b11efdc08755550170cfc984258daaac12abb
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-05-09 17:31:29 +0800

    feat: add `CharactersCollection`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const nums = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(nums);
-sorter.sort();
+const chars = new CharactersCollection("gkrhabdc");
+
+const numsSorter = new Sorter(nums);
+numsSorter.sort();
+
+const charsSorter = new Sorter(chars);
+charsSorter.sort();
+
 console.log(nums);
+console.log(chars);
