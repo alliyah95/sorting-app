ac7579787dd252acc1140d74c49aed6a41fae76b
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-05-09 20:59:47 +0800

    feat: add `LinkedList`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const nums = new NumbersCollection([10, 3, -5, 0]);
 const chars = new CharactersCollection("gkrhabdc");
@@ -11,5 +12,14 @@ numsSorter.sort();
 const charsSorter = new Sorter(chars);
 charsSorter.sort();
 
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+
 console.log(nums);
 console.log(chars);
+console.log(linkedList);
